675b6913d6a1b1daf37defe9c6b4b679bd9b2659
MickeyShide <mickeyshide@flowerave.app>
2025-11-30 23:27:30 +0300

    fix: add include router

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,7 +1,9 @@
 from .auth import router as auth_router
 from .locations import router as locations_router
+from .rooms import router as rooms_router
 
 __all__ = [
     auth_router,
-    locations_router
+    locations_router,
+    rooms_router,
 ]
