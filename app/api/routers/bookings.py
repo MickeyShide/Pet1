82ec0c4ac35f56b23d791ab9ddb1cbd21ca6ec31
MickeyShide <mickey_shide@list.ru>
2025-12-01 22:57:35 +0300

    fix: add route

--- app/api/routers/bookings.py
+++ app/api/routers/bookings.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter
 from starlette import status
 
 from app.api.deps import UserDepends
-from app.schemas.booking import SBookingOut, SBookingCreate, SBookingOutAfterCreate
+from app.schemas.booking import SBookingOut, SBookingCreate, SBookingOutAfterCreate, SBookingOutWithTimeslots, \
+    SBookingFilters
 from app.services.business.bookings import BookingsBusinessService
 
 router = APIRouter(prefix="/bookings", tags=["Bookings"])
@@ -19,3 +22,11 @@ async def create_booking_route(
         token_data: UserDepends
 ) -> SBookingOutAfterCreate:
     return await BookingsBusinessService(token_data=token_data).create_booking(booking_data)
+
+@router.get(
+    path="/",
+    status_code=status.HTTP_200_OK,
+    response_model=List[SBookingOutWithTimeslots],
+    description="Get all user bookings with optional filters", )
+async def get_all_user_bookings(token_data: UserDepends, booking_filters: SBookingFilters | None = None) -> List[SBookingOutWithTimeslots]:
+    return await BookingsBusinessService(token_data).get_my_bookings(booking_filters)
