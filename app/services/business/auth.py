2a46ddb448d2e37c6862f46fa7b6a95db084901f
MickeyShide <mickey_shide@list.ru>
2026-01-04 15:21:18 +0300

    fix: hot-kostil

--- app/services/business/auth.py
+++ app/services/business/auth.py
@@ -50,7 +50,7 @@ class AuthBusinessService(BaseBusinessService):
             value=refresh_token,
             httponly=True,
             secure=settings.COOKIE_SECURE,
-            samesite="lax",
+            samesite="none",
             max_age=settings.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60,
             #path="/auth/refresh",
         )
